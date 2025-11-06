0e0842e3a8406d3be52b245fb296bf4139f97e2a
ahe <tottilafrite@protonmail.com>
2025-11-06 16:43:34 +0100

    finish downloadfile part

--- etl_pipeline/etl_pipeline.py
+++ etl_pipeline/etl_pipeline.py
@@ -1,21 +1,36 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import re
-
+import os
+from tqdm import tqdm
 
 def get_csv_from_url(URL):
-    soup = BeautifulSoup(requests.get(URL).text)
+    soup = BeautifulSoup(requests.get(URL).text, features="html.parser")
     table = soup.table
 
     links = table.find_all("a")
-    file_names = []
+    online_file_names = []
+    file_path = './data/'
+    downloaded_files = os.listdir(file_path)
 
     for l in links:
         file_name = l.get('href')
         if "scribus" in file_name:
-            file_names.append(file_name)
+            if file_name not in downloaded_files:
+                try : 
+                    response = requests.get(URL + file_name)
+                    response.raise_for_status()
 
+                    total_size = int(response.headers.get('content-length', 0))
+
+                    with open(file_path + file_name, 'wb') as file, \
+                        tqdm(desc=file_name, total=total_size, unit='iB', unit_scale=True) as progress_bar:
+                        for chunk in response.iter_content(chunk_size=8192):
+                            size = file.write(chunk)
+                            progress_bar.update(size)
+                except requests.exceptions.RequestException as e:
+                    print(f"Error when downloading file {file_name} : {e}")
+                    
 
 def get_data_from_file(file_path):
     data = pd.read_csv(file_path)
@@ -39,6 +54,4 @@ def clean_data(data):
     return data
 
 
-df = get_data_from_file('data/scribus-dump-2025-11-03.csv')
-cleaned_df = clean_data(df)
 get_csv_from_url('http://teachingse.hevs.ch/csvFiles/')
